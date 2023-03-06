3a0f0eb8153145d5c0c2b043c37c8e2e91a3b7ef
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-06 12:23:55 -0800

    Clean up code

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,18 +7,21 @@ local function contain(tab, val)
 	return false
 end
 
+local function disalbeFormatting(client)
+	client.server_capabilities.document_formatting = false
+	client.server_capabilities.document_range_formatting = false
+end
+
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.colorProvider = {
 	dynamicRegistration = true,
 }
-local servers = { "bashls", "clangd", "html", "jsonls", "prismals", "pyright", "tailwindcss", "tsserver" }
-for _, lsp in pairs(servers) do
+for _, lsp in pairs({ "bashls", "clangd", "html", "jsonls", "prismals", "pyright", "tailwindcss", "tsserver" }) do
 	require("lspconfig")[lsp].setup({
 		capabilities = capabilities,
 		on_attach = function(client)
 			if contain({ "html", "jsonls", "tsserver" }, client.name) then
-				client.server_capabilities.document_formatting = false
-				client.server_capabilities.document_range_formatting = false
+				disalbeFormatting(client)
 			end
 			if client.server_capabilities.colorProvider then
 				require("document-color").buf_attach()
@@ -36,10 +39,7 @@ local lspconfig = require("lspconfig")
 
 lspconfig.lua_ls.setup({
 	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
-	end,
+	on_attach = disalbeFormatting,
 	cmd = { lua_ls, "-E", vim.fn.fnamemodify(lua_ls, ":h:h:h") .. "/main.lua" },
 	settings = {
 		Lua = {
@@ -76,10 +76,7 @@ require("typescript").setup({})
 
 lspconfig.rust_analyzer.setup({
 	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
-	end,
+	on_attach = disalbeFormatting,
 	settings = {
 		["rust-analyzer"] = {
 			-- assist = {
@@ -102,8 +99,7 @@ lspconfig.rust_analyzer.setup({
 	},
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
+for type, icon in pairs({ Error = " ", Warn = " ", Hint = " ", Info = " " }) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
