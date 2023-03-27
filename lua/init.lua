6c193c466f64f4fe836e2174081bb580f568a86c
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-27 11:47:07 -0700

    Updated mappings for codeium

--- lua/init.lua
+++ lua/init.lua
@@ -28,11 +28,18 @@ require("lazy").setup({
 	{
 		"Exafunction/codeium.vim",
 		config = function()
+			vim.g.codeium_disable_bindings = 1
 			vim.keymap.set("i", "<M-p>", function()
 				return vim.fn["codeium#Accept"]()
 			end, { expr = true })
 			vim.keymap.set("i", "<M-x>", function()
 				return vim.fn["codeium#Clear"]()
+			end, { expr = true })
+			vim.keymap.set("i", "<M-]>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<M-[>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
 			end, { expr = true })
 		end,
 	},
