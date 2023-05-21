026434d4fe530ef154966ff727556d36a8144053
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-05-20 17:35:18 -0700

    feat(lsp): add borders to floating windows

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,6 +12,24 @@ local function disalbeFormatting(client)
 	client.server_capabilities.document_range_formatting = false
 end
 
+local border = {
+	{ "┌", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "┐", "FloatBorder" },
+	{ "│", "FloatBorder" },
+	{ "┘", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "└", "FloatBorder" },
+	{ "│", "FloatBorder" },
+}
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.colorProvider = {
 	dynamicRegistration = true,
