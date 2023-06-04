a8b5ee81be1f9ced7c38f628b88255562ae3d1a2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-06-04 08:53:25 -0700

    refactor(lazy): change plugins from string to string[]

--- lua/init.lua
+++ lua/init.lua
@@ -90,7 +90,7 @@ require("lazy").setup({
 		"glacambre/firenvim",
 		cond = not not vim.g.started_by_firenvim,
 		build = function()
-			require("lazy").load({ plugins = "firenvim", wait = true })
+			require("lazy").load({ plugins = { "firenvim" }, wait = true })
 			vim.fn["firenvim#install"](0)
 		end,
 	},
