4f82a39c16b6ed7dedac1b6a264c55f3d728edeb
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-07-16 18:04:09 -0700

    refactor(plugin): mv cursor and line outside of return value

--- after/plugin/codeium.lua
+++ after/plugin/codeium.lua
@@ -16,19 +16,14 @@ vim.keymap.set("i", "<M-[>", function()
   return vim.fn["codeium#CycleCompletions"](-1)
 end, { expr = true })
 
-local function create_setup()
-  return {
-    cursor = vim.api.nvim_win_get_cursor(0),
-    line = vim.api.nvim_get_current_line(),
-    text = vim.api.nvim_eval("b:_codeium_completions.items[b:_codeium_completions.index].completionParts[0].text"),
-  }
+local function text_request()
+  return vim.api.nvim_eval("b:_codeium_completions.items[b:_codeium_completions.index].completionParts[0].text")
 end
 
 vim.keymap.set("i", "<M-w>", function()
-  local setup = create_setup()
-  local cursor = setup.cursor
-  local line = setup.line
-  local text = string.match(setup.text, "[ ,;.]*[^ ,;.]+")
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line = vim.api.nvim_get_current_line()
+  local text = string.match(text_request(), "[ ,;.]*[^ ,;.]+")
   vim.defer_fn(function()
     if string.match(text, "^\t") then
       vim.api.nvim_buf_set_lines(0, cursor[1], cursor[1], true, { text })
@@ -41,10 +36,9 @@ vim.keymap.set("i", "<M-w>", function()
 end, { expr = true })
 
 vim.keymap.set("i", "<M-o>", function()
-  local setup = create_setup()
-  local cursor = setup.cursor
-  local line = setup.line
-  local text = string.gsub(setup.text, "\n.*$", "")
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line = vim.api.nvim_get_current_line()
+  local text = string.gsub(text_request(), "\n.*$", "")
   if text ~= "" then
     vim.defer_fn(function()
       vim.api.nvim_set_current_line(line:sub(0, cursor[2]) .. text .. line:sub(cursor[2] + 1))
