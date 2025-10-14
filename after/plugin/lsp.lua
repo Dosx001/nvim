31450ab072ac248afe5ec03137baf1e5fc6f96fc
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-10-14 09:43:46 -0700

    lint: sign_define deprecated

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -130,7 +130,19 @@ vim.lsp.config("rust_analyzer", {
   },
 })
 
-for type, icon in pairs({ Error = " ", Warn = " ", Hint = " ", Info = " " }) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+    },
+  },
+})
