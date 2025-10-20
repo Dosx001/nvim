43b37101fc4c5d5be9173a702d464c045383f599
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-10-20 10:12:02 -0700

    feat: setup dap gdb

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,11 +1,5 @@
 local dap, dapui = require("dap"), require("dapui")
 
-dap.adapters.python = {
-  type = "executable",
-  command = "python",
-  args = { "-m", "debugpy.adapter" },
-}
-
 dapui.setup({
   layouts = {
     {
@@ -32,6 +26,68 @@ dapui.setup({
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open({})
 end
+
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+
+local function getBinary()
+  local cwd = vim.fn.getcwd()
+  local name = vim.fn.fnamemodify(cwd, ":t")
+  if vim.fn.isdirectory(cwd .. "/zig-out") == 1 then
+    return cwd .. "/zig-out/bin/" .. name
+  elseif vim.fn.isdirectory(cwd .. "/build/debug") == 1 then
+    return cwd .. "/build/debug/" .. name
+  elseif vim.fn.isdirectory(cwd .. "/target") == 1 then
+    return cwd .. "/target/debug/" .. name
+  end
+  return vim.fn.input("Path to executable: ", cwd .. "/", "file")
+end
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = getBinary,
+    args = function()
+      return vim.fn.input({ prompt = "Arguments: " })
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+  {
+    name = "Select and attach to process",
+    type = "gdb",
+    request = "attach",
+    program = getBinary,
+    pid = function()
+      local name = vim.fn.input("Executable name (filter): ")
+      return require("dap.utils").pick_process({ filter = name })
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Attach to gdbserver :1234",
+    type = "gdb",
+    request = "attach",
+    target = "localhost:1234",
+    program = getBinary,
+    cwd = "${workspaceFolder}",
+  },
+}
+
+dap.configurations.cpp = dap.configurations.c
+dap.configurations.rust = dap.configurations.c
+dap.configurations.zig = dap.configurations.c
+
+dap.adapters.python = {
+  type = "executable",
+  command = "python",
+  args = { "-m", "debugpy.adapter" },
+}
 
 dap.configurations.python = {
   {
